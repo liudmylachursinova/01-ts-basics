cda8353b674111f9f9add970bb117bdc83e5c896
Liudmyla Chursinova <lyubahalik@gmail.com>
2026-09-20 00:11:53 +0200

    Tasks 1-5

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,1 +1,8 @@
-import './style.css'
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
+
