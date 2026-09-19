261aa066d5227d13a50cfeed3594ab1977959b6e
Liudmyla Chursinova <lyubahalik@gmail.com>
2026-09-20 00:29:04 +0200

    Tasks 5 and 6

--- src/task-5.ts
+++ src/task-5.ts
@@ -1,7 +1,7 @@
 type Status = "loading" | "success" | "error";
 
 
-function logStatus(status: Status) {
+function logStatus(status: Status): void {
   if (status === "loading") {
     console.log("Loading...");
   } else if (status === "success") {
